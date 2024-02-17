14fde5314b063020e3f7bf9262ba7e312220dad9
José Enrique de la Torre Leyva <37634439+untalkikin@users.noreply.github.com>
2024-02-16 21:39:16 -0600

    More updates
    more updates

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", cartRouter);
-app.use("/api", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/products", productRouter);
 
 app.listen(PORT, () => console.log(`El servidor esta escuchando en el: ${PORT}`))
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,20 +1,11 @@
 import { Router } from "express";
 import ProductManager from "../ProductManager.js"
 
-const productManager = new ProductManager ("productos.json");
-const router = Router();
+const productManager = new ProductManager ();
+const productRouter = Router();
 
 //Rutas
 
-router.get("/", (req, res) => {});
-
-router.post("/", (req, res) => {});
-
-router.get("/:pid", (req, res) => {});
-
-router.put("/:pid", (req, res) => {});
-
-router.delete("/:pid", (req, res) => {});
 
 
-export default router;
+export default productRouter;
